4d826149e7af30fb210bbc445fbee139594bdd3b
andrew.lam@rangle.io <andrew.lam@rangle.io>
2020-08-30 17:06:44 -0400

    Fix recipe ingredient updates

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,17 +2,16 @@ import database from '@react-native-firebase/database';
 
 const RECIPES_REF = 'recipes';
 
-export const init = () => {
-  database().ref(RECIPES_REF).remove();
+// export const init = () => {
+//   database().ref(RECIPES_REF).remove();
 
-  database().ref(RECIPES_REF).push({});
-};
+//   database().ref(RECIPES_REF).push({});
+// };
 
 export const addRecipe = (name) => database().ref(RECIPES_REF).push().update({ name });
 export const updateRecipeIngredients = (recipeId, ingredients) => {
   database()
-    .ref(RECIPES_REF)
-    .once(recipeId)
+    .ref(`${RECIPES_REF}/${recipeId}`)
     .update({ ingredients: ingredients.map((ingredient) => ingredient.toLowerCase()) });
 };
 export const getAllRecipes = () =>
