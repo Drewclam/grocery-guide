673255d996459a3579eab137b17ab7b334663a7c
andrew.lam@rangle.io <andrew.lam@rangle.io>
2020-08-30 15:18:54 -0400

    Add new ingredient adds to existing recipe

--- src/screens/RecipeDetailScreen.js
+++ src/screens/RecipeDetailScreen.js
@@ -1,5 +1,13 @@
-import React, { useState } from 'react';
-import { SafeAreaView, TextInput, StyleSheet, FlatList, Text } from 'react-native';
+import React, { useState, useRef } from 'react';
+import {
+  SafeAreaView,
+  TextInput,
+  StyleSheet,
+  FlatList,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 
 import { spacing, fontSize } from '../utils/styles';
 
@@ -11,40 +19,70 @@ const RecipeDetailScreen = ({ navigation }) => {
   } = navigation;
   const [value, setValue] = useState(recipe.name);
   const [ingredients, setIngredients] = useState(recipe.ingredients);
+  const [autoFocus, setAutoFocus] = useState(false);
 
   const RecipeDetailRenderItem = ({ item }) => (
-    <TextInput
-      value={item.name}
-      multiline={true}
-      blurOnSubmit={true}
-      onChangeText={(text) => {
-        let newIngredients = ingredients.map((ingredient) =>
-          ingredient.id === item.id
-            ? {
-                ...ingredient,
-                name: text,
-              }
-            : ingredient,
-        );
-        setIngredients(newIngredients);
-      }}
-    />
-  );
-
-  return (
-    <SafeAreaView style={styles.container}>
+    <View>
       <TextInput
-        style={styles.title}
-        value={value}
-        onChangeText={(text) => setValue(text)}
+        style={{ backgroundColor: 'red' }}
+        value={item.name}
         multiline={true}
         blurOnSubmit={true}
+        autoFocus={autoFocus}
+        onChangeText={(text) => {
+          let newIngredients = ingredients.map((ingredient) =>
+            ingredient.id === item.id
+              ? {
+                  ...ingredient,
+                  name: text,
+                }
+              : ingredient,
+          );
+          setIngredients(newIngredients);
+        }}
       />
-      <FlatList
-        data={ingredients}
-        renderItem={RecipeDetailRenderItem}
-        keyExtractor={(ingredient) => ingredient.id}
-      />
+      {/* Todo: Render editable quantity. */}
+    </View>
+  );
+
+  const hasEmptyIngredient = () => {
+    const newIngredient = ingredients[ingredients.length - 1];
+    if (!newIngredient.name || newIngredient.name === '') {
+      return true;
+    }
+    return false;
+  };
+
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.title}
+          value={value}
+          onChangeText={(text) => setValue(text)}
+          multiline={true}
+          blurOnSubmit={true}
+        />
+        <FlatList
+          data={ingredients}
+          renderItem={RecipeDetailRenderItem}
+          keyExtractor={(ingredient) => ingredient.id}
+        />
+      </View>
+      <TouchableOpacity
+        style={{ height: '100%' }}
+        onPress={() => {
+          if (hasEmptyIngredient()) {
+            setIngredients(
+              ingredients.filter((ingredient) => ingredient.name && ingredient.name !== ''),
+            );
+            setAutoFocus(false);
+          } else {
+            setIngredients([...ingredients, {}]);
+            setAutoFocus(true);
+          }
+        }}
+      ></TouchableOpacity>
     </SafeAreaView>
   );
 };
