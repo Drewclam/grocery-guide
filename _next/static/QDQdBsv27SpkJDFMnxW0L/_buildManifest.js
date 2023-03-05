9d18953c0ae17555df7ef73c071d2ddf24bc0c42
Drewclam <andrew.c.lam94@gmail.com>
2023-03-05 15:51:53 +0000

    Deploying to public from @ Drewclam/grocery-guide@eb112b9ea3eba9de5c6c50e53841ee4e09b99e17 🚀

---
+++ _next/static/QDQdBsv27SpkJDFMnxW0L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0081e48663501b48.css","static/chunks/pages/index-5e07e48a94dd6c9a.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
